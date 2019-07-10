63d4c44725509eef942b6cb0f043f2f637979b9a
DELPHI-BERLIN\russell <russell@smxi.com>
2019-07-10 16:02:50 +0100

    initial commit

---
+++ Unit1.dfm
@@ -0,0 +1,224 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 557
+  ClientWidth = 922
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  DesignSize = (
+    922
+    557)
+  PixelsPerInch = 96
+  TextHeight = 13
+  object ListBox1: TListBox
+    Left = 24
+    Top = 72
+    Width = 402
+    Height = 329
+    ItemHeight = 13
+    MultiSelect = True
+    TabOrder = 0
+  end
+  object PlatformSelect: TComboBox
+    Left = 24
+    Top = 45
+    Width = 402
+    Height = 21
+    TabOrder = 1
+    Text = 'PlatformSelect'
+    OnCloseUp = PlatformSelectCloseUp
+    Items.Strings = (
+      'Win32'
+      'Win64'
+      'OSX32'
+      'Linux64'
+      'iOSSimulator'
+      'iOSDevice64'
+      'iOSDevice32'
+      'Android32')
+  end
+  object Memo1: TMemo
+    Left = 488
+    Top = 64
+    Width = 417
+    Height = 329
+    Anchors = [akLeft, akTop, akRight]
+    Lines.Strings = (
+      'Memo1')
+    TabOrder = 2
+    WordWrap = False
+  end
+  object Platforms: TCheckListBox
+    Left = 384
+    Top = 430
+    Width = 225
+    Height = 58
+    Columns = 2
+    ItemHeight = 13
+    Items.Strings = (
+      'Win32'
+      'Win64'
+      'OSX32'
+      'Linux64'
+      'iOSSimulator'
+      'iOSDevice64'
+      'iOSDevice32'
+      'Android32')
+    TabOrder = 3
+  end
+  object Button1: TButton
+    Left = 800
+    Top = 407
+    Width = 75
+    Height = 25
+    Caption = 'Register'
+    TabOrder = 4
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 432
+    Top = 216
+    Width = 34
+    Height = 25
+    Caption = '>>'
+    TabOrder = 5
+    OnClick = Button2Click
+  end
+  object IgnorePath: TCheckBox
+    Left = 624
+    Top = 407
+    Width = 153
+    Height = 17
+    Caption = 'Ignore if Path doesn'#39't exist'
+    Checked = True
+    State = cbChecked
+    TabOrder = 6
+  end
+  object SortNew: TCheckBox
+    Left = 624
+    Top = 428
+    Width = 97
+    Height = 17
+    Caption = 'Sort New Entries'
+    Checked = True
+    State = cbChecked
+    TabOrder = 7
+  end
+  object SortLibrary: TCheckBox
+    Left = 624
+    Top = 471
+    Width = 97
+    Height = 17
+    Caption = 'Sort Library Paths'
+    TabOrder = 8
+  end
+  object RemoveAndReplace: TCheckBox
+    Left = 624
+    Top = 449
+    Width = 153
+    Height = 17
+    Caption = 'Remove and Replace'
+    Checked = True
+    State = cbChecked
+    TabOrder = 9
+  end
+  object SortNewButton: TButton
+    Left = 830
+    Top = 33
+    Width = 75
+    Height = 25
+    Caption = 'Sort && Clean'
+    TabOrder = 10
+    OnClick = SortNewButtonClick
+  end
+  object Button4: TButton
+    Left = 24
+    Top = 424
+    Width = 75
+    Height = 25
+    Caption = 'Sort'
+    TabOrder = 11
+  end
+  object CleanUpButton: TButton
+    Left = 749
+    Top = 33
+    Width = 75
+    Height = 25
+    Caption = 'Clean Up'
+    TabOrder = 12
+    OnClick = CleanUpButtonClick
+  end
+  object Button3: TButton
+    Left = 569
+    Top = 33
+    Width = 75
+    Height = 25
+    Action = FileSaveAs1
+    TabOrder = 13
+  end
+  object Button5: TButton
+    Left = 488
+    Top = 33
+    Width = 75
+    Height = 25
+    Action = FileOpen1
+    TabOrder = 14
+  end
+  object VersionSelect: TCheckListBox
+    Left = 384
+    Top = 408
+    Width = 225
+    Height = 16
+    Columns = 4
+    Ctl3D = False
+    ItemHeight = 13
+    Items.Strings = (
+      'Rio'
+      'Seattle')
+    ParentCtl3D = False
+    TabOrder = 15
+  end
+  object VersionBase: TRadioGroup
+    Left = 24
+    Top = 0
+    Width = 185
+    Height = 41
+    Caption = 'Delphi Version'
+    Columns = 3
+    Items.Strings = (
+      'Rio'
+      'Tokyo')
+    TabOrder = 16
+  end
+  object ActionList1: TActionList
+    Left = 448
+    Top = 280
+    object FileSaveAs1: TFileSaveAs
+      Category = 'File'
+      Caption = 'Save'
+      Dialog.DefaultExt = 'txt'
+      Dialog.Filter = 'Text File (*.txt)|*.txt'
+      Hint = 'Save As|Saves the active file with a new name'
+      ImageIndex = 30
+      OnAccept = FileSaveAs1Accept
+    end
+    object FileOpen1: TFileOpen
+      Category = 'File'
+      Caption = '&Open...'
+      Dialog.DefaultExt = 'txt'
+      Dialog.Filter = 'Text File (*.txt)|*.txt'
+      Hint = 'Open|Opens an existing file'
+      ImageIndex = 7
+      ShortCut = 16463
+      OnAccept = FileOpen1Accept
+    end
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,336 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.CheckLst,
+  System.ImageList, Vcl.ImgList, Vcl.ExtCtrls, System.Actions, Vcl.ActnList,
+  Vcl.StdActns;
+
+type
+  {$SCOPEDENUMS ON}
+  TDelphiVersion = (Tokyo=19,Rio=20);
+  {$SCOPEDENUMS OFF}
+
+  TForm1 = class(TForm)
+    ListBox1: TListBox;
+    PlatformSelect: TComboBox;
+    Memo1: TMemo;
+    Platforms: TCheckListBox;
+    Button1: TButton;
+    Button2: TButton;
+    IgnorePath: TCheckBox;
+    SortNew: TCheckBox;
+    SortLibrary: TCheckBox;
+    RemoveAndReplace: TCheckBox;
+    SortNewButton: TButton;
+    Button4: TButton;
+    CleanUpButton: TButton;
+    Button3: TButton;
+    ActionList1: TActionList;
+    FileSaveAs1: TFileSaveAs;
+    FileOpen1: TFileOpen;
+    Button5: TButton;
+    VersionSelect: TCheckListBox;
+    VersionBase: TRadioGroup;
+    procedure FormDestroy(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure SortNewButtonClick(Sender: TObject);
+    procedure CleanUpButtonClick(Sender: TObject);
+    procedure FileOpen1Accept(Sender: TObject);
+    procedure FileSaveAs1Accept(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure PlatformSelectCloseUp(Sender: TObject);
+  private
+    FVersionList: TStrings;
+    function StringToVersion(const Value: string): TDelphiVersion;
+    function LibraryKey(AVersion: string): string;
+    procedure LoadAvailableVersions;
+    function LookUpVersionName(const Value: string): string;
+    procedure WriteToTheRegistry(AVersion, APlatform: string; lList: TStringList);
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+uses System.Win.Registry, System.IOUtils, System.Rtti;
+
+
+resourcestring
+  SSearchPath = 'Search Path';
+  SRootPath =  '\Software\Embarcadero\BDS\';
+//  SLibraryRegistryKey = '\Software\Embarcadero\BDS\20.0\Library\';
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  FVersionList.Free;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+var lList: TStringList;
+  I, J: Integer;
+  lPath: string;
+begin
+  lList := TStringList.Create;
+  try
+    for I := 0 to Memo1.Lines.Count - 1 do
+    begin
+      lPath := Memo1.Lines[I];
+      if IgnorePath.Checked and (not TDirectory.Exists(lPath)) then
+         Continue;
+      //don't need dupes
+      if lList.IndexOf(lPath) < 0 then
+         lList.Add(lPath);
+    end;
+
+    if SortNew.Checked then
+       lList.Sort;
+
+    for J := 0 to VersionSelect.Count - 1 do
+    begin
+      if VersionSelect.Checked[J] then
+      begin
+        for I := 0 to Platforms.Count - 1 do
+        begin
+          if Platforms.Checked[I] then
+          begin
+            WriteToTheRegistry(VersionSelect.Items[J], Platforms.Items[I], lList);
+          end;
+        end;
+      end;
+    end;
+  finally
+    lList.Free;
+  end;
+
+  ShowMessage('Finished');
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var I: Integer;
+begin
+  for I := 0 to ListBox1.Count - 1 do
+  begin
+    if ListBox1.Selected[I] then
+       Memo1.Lines.Add(ListBox1.Items[I]);
+  end;
+end;
+
+procedure TForm1.SortNewButtonClick(Sender: TObject);
+var ts: TStringList;
+begin
+  ts := TStringList.Create;
+  try
+    ts.AddStrings(Memo1.Lines);
+    ts.Sort;
+    Memo1.Lines.Clear;
+    Memo1.Lines.Assign(ts);
+  finally
+    ts.Free;
+  end;
+  CleanUpButton.Click;
+end;
+
+function TForm1.StringToVersion(const Value: string): TDelphiVersion;
+begin
+  Result := TRttiEnumerationType.GetValue<TDelphiVersion>(Value);
+end;
+
+procedure TForm1.CleanUpButtonClick(Sender: TObject);
+var ts: TStringList;
+  I: Integer;
+begin
+  ts := TStringList.Create;
+  try
+    for I := 0 to Memo1.Lines.Count - 1 do
+    begin
+      if (Memo1.Lines[I].Trim = '')  or (ts.IndexOf(Memo1.Lines[I]) >= 0) then
+         Continue;
+
+      ts.Add(Memo1.Lines[I]);
+
+    end;
+
+   Memo1.Lines.Clear;
+   Memo1.Lines.Assign(ts);
+
+  finally
+    ts.Free;
+  end;
+
+end;
+
+procedure TForm1.FileOpen1Accept(Sender: TObject);
+begin
+  Memo1.Lines.LoadFromFile(FileOPen1.Dialog.FileName);
+end;
+
+procedure TForm1.FileSaveAs1Accept(Sender: TObject);
+begin
+  Memo1.Lines.SaveToFile(FileSaveAs1.Dialog.FileName);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  ListBox1.Items.Delimiter := ';';
+  ListBox1.Items.StrictDelimiter := True;
+  Memo1.Clear;
+  FVersionList := TStringList.Create;
+end;
+
+function TForm1.LibraryKey(AVersion: string): string;
+begin
+  result := SRootPath +
+    Integer(StringToVersion(AVersion)).ToString + '.0\Library\'
+end;
+
+procedure TForm1.LoadAvailableVersions;
+var Registry: TRegistry;
+  I: Integer;
+  lName: string;
+begin
+    VersionBase.Items.Clear;
+    VersionSelect.Items.Clear;
+
+    Registry := TRegistry.Create;
+    try
+      Registry.RootKey := HKEY_CURRENT_USER;
+      if not Registry.OpenKey(SRootPath, False) then
+      begin
+        Registry.GetKeyNames(FVersionList);
+      end;
+    finally
+      Registry.Free;
+    end;
+
+    TStringList(FVersionList).Sort;
+
+    for I := 0 to FVersionList.Count - 1 do
+    begin
+      lName := LookUpVersionName(FVersionList[I]);
+      if lName = '' then
+         Continue;
+
+      FVersionList[I] := lName + '=' + FVersionList[I];
+      VersionBase.Items.Add(lName);
+    end;
+
+    VersionSelect.Items.AddStrings(VersionBase.Items);
+    if VersionSelect.Count = 1 then
+    begin
+      VersionSelect.ItemIndex := 0;
+      VersionBase.ItemIndex := 0;
+    end;
+end;
+
+function TForm1.LookUpVersionName(const Value: string): string;
+var lValue: Integer;
+    lHigh, lLow: SmallInt;
+    lVersion: TDelphiVersion;
+    lVal: string;
+begin
+
+  Result := '';
+  lVal := Value.Substring(0, Value.IndexOf('.') - 1);
+  lValue := StrToIntDef(lVal, 0);
+
+  lHigh := Ord(High(TDelphiVersion));
+  lLow := Ord(Low(TDelphiVersion));
+  if ((lValue > lHigh) or (lValue < lLow)) then
+     Exit;
+
+  lVersion := TDelphiVersion(lValue);
+
+  result := TRttiEnumerationType.GetName<TDelphiVersion>(lVersion);
+
+end;
+
+procedure TForm1.PlatformSelectCloseUp(Sender: TObject);
+var lPlatform: string;
+    Registry: TRegistry;
+begin
+  if VersionBase.ItemIndex < 0 then
+  begin
+    ShowMessage('Choose a base first');
+    PlatformSelect.ItemIndex := -1;
+    Exit;
+  end;
+
+  lPlatform := PlatformSelect.Items[PlatformSelect.ItemIndex];
+  ListBox1.Clear;
+
+  Registry := TRegistry.Create;
+  try
+    Registry.RootKey := HKEY_CURRENT_USER;
+    Registry.OpenKeyReadOnly(LibraryKey(VersionBase.Items[VersionBase.ItemIndex]) + lPlatform);
+    ListBox1.Items.DelimitedText :=  Registry.ReadString(SSearchPath);
+  finally
+    Registry.Free;
+  end;
+end;
+
+procedure TForm1.WriteToTheRegistry(AVersion, APlatform: string; lList: TStringList);
+var Registry: TRegistry;
+    lPaths: TStringList;
+  I, P: Integer;
+begin
+  lPaths := TStringList.Create;
+  try
+    lPaths.Delimiter := ';';
+    lPaths.StrictDelimiter := True;
+    Registry := TRegistry.Create;
+    try
+     Registry.RootKey := HKEY_CURRENT_USER;
+     if not Registry.OpenKey(LibraryKey(AVersion) + APlatform, False) then
+        raise Exception.Create(LibraryKey(AVersion) + APlatform + ' does not exist');
+
+     lPaths.DelimitedText := Registry.ReadString(SSearchPath);
+
+     if RemoveAndReplace.Checked then
+     begin
+       for I := 0 to lList.Count - 1 do
+       begin
+
+         if lList[I].Trim = '' then Continue;
+
+         P := lPaths.IndexOf(lList[I]);
+         if P >= 0 then
+            lPaths.Delete(P);
+
+       end;
+     end;
+
+     for I := 0 to lList.Count - 1 do
+     begin
+       if lList[I].Trim = '' then Continue;
+
+       if (lPaths.IndexOf(lList[I]) > -1) then
+          continue;
+
+       lPaths.Add(lList[I]);
+
+     end;
+
+     if SortLibrary.Checked then
+        lPaths.Sort;
+
+     Registry.WriteString(SSearchPath, lPaths.DelimitedText);
+     
+    finally
+      Registry.Free;
+    end;
+  finally
+    lPaths.Free;
+  end;
+end;
+
+end.
